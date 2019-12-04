796a62c976d2c6feb6086aa0b9d267cc27c5d906
Emma <emmahlowe@gmail.com>
2019-12-04 10:15:29 -0700

    Added Models

---
+++ IntexMVC/Models/Assays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IntexMVC.Models
+{
+    [Table("Assays")]
+
+    public class Assays
+    {
+        [Key]
+        public int LT_Num { get; set; }
+
+        public int Assay_Description_ID { get; set; }
+
+        public int Sequence_Code { get; set; }
+
+        public string Raw_Qualitative_Results { get; set; }
+
+        public string Raw_Quantitative_Results { get; set; }
+
+        public string Analysis_Reports { get; set; }
+    }
+}
